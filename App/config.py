bc4fada04c56906307cb989d526895ec64d2a013
xCoolHat <simfon@protonmail.ch>
2022-01-10 00:33:42 +0000

    Some more changes II

---
+++ App/config.py
@@ -0,0 +1,12 @@
+# ************************CONFIG*************************************************
+# Put your API Key here
+KEY = None
+# Cache Paths ---> include the name of the file, example: 'cache_coin.json' or 'caches/cache_coin.json'
+#
+# WARNING: it assumes the whole path exists, except the file that may be overwritten if it already exists or created if it doesn't
+#
+# Path where the coin info cache file will be stored
+CACHE_COIN_PATH = ''
+# Path where the fiat coin info cache file will be stored
+CACHE_FIAT_PATH = ''
+# *******************************************************************************
